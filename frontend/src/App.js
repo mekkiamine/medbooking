3667e7164b1b462a407409045f7ef26336474545
mekki amine <mekkiamine34@gmail.com>
2023-12-04 11:15:06 +0100

    Signup Finished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import './login';
 import './SignInDoc';
+import './SignInPat';
 import { Login } from './login';
 import { SignInDoc } from './SignInDoc';
+import { SignInPat } from './SignInPat';
 
 function App() {
   return (
     <div className="App">
-     <SignInDoc/>
+     <SignInPat/>
     </div>
   );
 }
--- frontend/src/SignInDoc.jsx
+++ frontend/src/SignInDoc.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './signin.css';
 
-import { Menubar } from 'primereact/menubar';
-        
 export const SignInDoc = () => {
-    
-    return (
-       <>
+  const [formData, setFormData] = useState({
+    nom: '',
+    prenom: '',
+    adresse: '',
+    tel: '',
+    email: '',
+    mot_de_passe: '',
+    matricule: '',
+    specialite: '',
+    hopital: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData)
+    try {
+      const response = await fetch('medecin/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Medecin registered successfully');
+        // Optionally, redirect or show a success message
+      } else {
+        console.error('Failed to register medecin');
+        // Handle error, show error message, etc.
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <>
+      {/* ... existing imports ... */}
+
       <link
         rel="stylesheet"
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
       <body>
         <section className="login">
@@ -21,36 +61,81 @@ export const SignInDoc = () => {
                 <h3>Welcome To MedBooking</h3>
               </div>
               <div className="contact">
-                <form action="">
+                <form onSubmit={handleSubmit}>
                   <h3>SIGN IN</h3>
-                  <input type="text" placeholder="Nom et Prénom" />
-                        <input type="password" placeholder="PASSWORD" />
-                        <input type="email" placeholder="EMAIL" />
-                        <input type="text" placeholder="ADDRESS" />
-                        <input type="tel" placeholder="PHONE NUMBER" />
-                        <input type="text" placeholder="CITY" />
-                        <input type="text" placeholder="MATRICULE" />
-                        <input type="text" placeholder="SPÉCIALITÉ" />
-                  <button className="submit">LET'S GO</button>
+                  <input
+                    type="text"
+                    name="nom"
+                    placeholder="Nom"
+                    value={formData.nom}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="text"
+                    name="prenom"
+                    placeholder="Prénom"
+                    value={formData.prenom}
+                    onChange={handleChange}
+                  />
+                  
+                  <input
+                    type="text"
+                    name="adresse"
+                    placeholder="Adresse"
+                    value={formData.adresse}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="tel"
+                    name="tel"
+                    placeholder="Téléphone"
+                    value={formData.tel}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="password"
+                    name="mot_de_passe"
+                    placeholder="Mot de passe"
+                    value={formData.mot_de_passe}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="text"
+                    name="matricule"
+                    placeholder="Matricule"
+                    value={formData.matricule}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="text"
+                    name="specialite"
+                    placeholder="Spécialité"
+                    value={formData.specialite}
+                    onChange={handleChange}
+                  />
+                  <input
+                    type="text"
+                    name="hopital"
+                    placeholder="Hopital"
+                    value={formData.hopital}
+                    onChange={handleChange}
+                  />
+                  <button type="submit" className="submit">
+                    LET'S GO
+                  </button>
                 </form>
-              </div>
-            </div>
-            <div className="right">
-              <div className="right-text">
-                <h2>LONYX</h2>
-                <h5>A UX BASED CREATIVE AGENCY</h5>
-              </div>
-              <div className="right-inductor">
-                <img
-                  src="https://lh3.googleusercontent.com/fife/ABSRlIoGiXn2r0SBm7bjFHea6iCUOyY0N2SrvhNUT-orJfyGNRSMO2vfqar3R-xs5Z4xbeqYwrEMq2FXKGXm-l_H6QAlwCBk9uceKBfG-FjacfftM0WM_aoUC_oxRSXXYspQE3tCMHGvMBlb2K1NAdU6qWv3VAQAPdCo8VwTgdnyWv08CmeZ8hX_6Ty8FzetXYKnfXb0CTEFQOVF4p3R58LksVUd73FU6564OsrJt918LPEwqIPAPQ4dMgiH73sgLXnDndUDCdLSDHMSirr4uUaqbiWQq-X1SNdkh-3jzjhW4keeNt1TgQHSrzW3maYO3ryueQzYoMEhts8MP8HH5gs2NkCar9cr_guunglU7Zqaede4cLFhsCZWBLVHY4cKHgk8SzfH_0Rn3St2AQen9MaiT38L5QXsaq6zFMuGiT8M2Md50eS0JdRTdlWLJApbgAUqI3zltUXce-MaCrDtp_UiI6x3IR4fEZiCo0XDyoAesFjXZg9cIuSsLTiKkSAGzzledJU3crgSHjAIycQN2PH2_dBIa3ibAJLphqq6zLh0qiQn_dHh83ru2y7MgxRU85ithgjdIk3PgplREbW9_PLv5j9juYc1WXFNW9ML80UlTaC9D2rP3i80zESJJY56faKsA5GVCIFiUtc3EewSM_C0bkJSMiobIWiXFz7pMcadgZlweUdjBcjvaepHBe8wou0ZtDM9TKom0hs_nx_AKy0dnXGNWI1qftTjAg=w1920-h979-ft"
-                  alt=""
-                />
               </div>
             </div>
           </div>
         </section>
       </body>
     </>
-    );
-  };
-  
+  );
+};
